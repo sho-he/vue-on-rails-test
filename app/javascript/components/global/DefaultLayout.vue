1c814d5644e2d0ac835ba8a5d04b0c3792d3ec37
sho-he <s.n.0704.g@gmail.com>
2022-10-23 21:28:02 +0900

    globalファイル作成（未使用)

---
+++ app/javascript/components/global/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DefaultLayout',
+    props: {
+      args: {
+        type: Object,
+        default: () => null
+      }
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
